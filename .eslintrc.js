7b06723a0008f6bed1feed8a3e8e863dc46c3c95
Jack Rose <jack.rose@fxdigital.uk>
2022-03-13 16:06:58 +0000

    removed react scripts and created a webpack config to serve and build

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,13 @@
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module"
+  },
   plugins: [
     "@typescript-eslint",
+    "react-hooks",
     "no-loops",
     "prettier"
   ],
@@ -22,6 +27,9 @@ module.exports = {
   rules: {
     "no-console": 1,
     "no-loops/no-loops": 2,
-    "prettier/prettier": 2
+    "prettier/prettier": 2,
+    "react-hooks/rules-of-hooks": 2,
+    "react-hooks/exhaustive-deps": 1,
+    "react/prop-types": 0
   }
 }
---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const Dotenv = require('dotenv-webpack');
+const { ProvidePlugin } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: path.resolve(__dirname, './src/index.tsx')
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/'
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'jsx',
+          target: 'es2015'
+        }
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /node_modules/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'tsx',
+          target: 'es2015'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'css',
+              minify: true
+            }
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx', '.ts', '.tsx', '.css'],
+    alias: {}
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html')
+    }),
+    new ProvidePlugin({
+      React: 'react'
+    }),
+    new Dotenv()
+  ],
+  devServer: {
+    static: path.resolve(__dirname, './dist'),
+    historyApiFallback: true,
+    open: true,
+    compress: true,
+    hot: true,
+    port: 8080
+  }
+};
