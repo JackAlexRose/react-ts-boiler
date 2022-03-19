5dfbb4b875e35fbc2d26bca80627ce876d6de99d
Jack Rose <jack.rose@fxdigital.uk>
2022-03-19 12:32:38 +0000

    chore: cleaned up dependancies and removed prop types

--- setupTests.js
+++ setupTests.js
@@ -2,8 +2,6 @@
 import '@testing-library/jest-dom'
 import React from 'react'
 import ReactDom from 'react-dom'
-import PropTypes from 'prop-types'
 
 global.React = React
 global.ReactDom = ReactDom
-global.PropTypes = PropTypes
